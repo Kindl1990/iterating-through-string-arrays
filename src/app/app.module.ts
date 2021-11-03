4c3b13b40a506973041c6dd41018655ebbeda824
Jiří Kindl <kindljiri1990@gmail.com>
2021-11-03 11:49:23 +0100

    Iterating through array declared in component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+/* The Angular CLI generates the following basic AppModule when creating a new application. */
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent    /* declarations - which component belong to NgModule*/
   ],
   imports: [
-    BrowserModule,
+    BrowserModule,  /* imports - which other modules NgModule uses */
     AppRoutingModule
   ],
   providers: [],
