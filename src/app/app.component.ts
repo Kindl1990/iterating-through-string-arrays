d7b0a0dcf167db186edfe20a6f0689c8de92ff7d
Jiří Kindl <kindljiri1990@gmail.com>
2021-11-03 14:07:27 +0100

    Iterating through interface own shape of data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Shoes } from './my-interface';   /* importing Shoes interface - data type from my-interface.ts file */
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,13 @@ export class AppComponent {
 
   /* array declared in component */
   programmingLanguages:string[] = ['Javascript', 'Python', 'Go', 'Java'];
+  
+  /* creating shoes variables of own data type Shoes */
+  /* interfaces are made up of various fields, similar to a JavaScript object. */
+  first_shoe: Shoes = { brand: 'Nike', color: 'black' };
+  second_shoe: Shoes = { brand: 'Addidas', color: 'white' };
+
+  /* array shoes of data type Shoes */
+  shoes:Shoes[ ] = [this.first_shoe, this.second_shoe];
 
 }
---
+++ src/app/my-interface.ts
@@ -0,0 +1,7 @@
+/* interface for shoes in component - template for own data shape */
+
+export interface Shoes {
+    brand: string;
+    color: string;
+  }
+
